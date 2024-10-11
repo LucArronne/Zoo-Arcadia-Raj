96f010132412dd297a2a2e406ded2972bf90d466
LucArronne <lucar.tservice@gmail.com>
2024-10-11 11:49:58 +0200

    Fix service page header and remove unnecessary route

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/service", "Service", "/pages/service.html"),];
+    new Route("/service", "Accueil", "/pages/service.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
