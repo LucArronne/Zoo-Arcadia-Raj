93525ab168a0ae362369177540ba776bc4a3645b
LucArronne <lucar.tservice@gmail.com>
2024-10-14 00:23:48 +0200

    Marais a finir

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,7 +5,10 @@ export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/service", "Les Services", "/pages/service.html"),
     new Route("/habitats", "Les Habitats", "/pages/habitats.html"),
-    new Route("/habitat-savane", "habitat-savane", "/pages/habitat-savane.html"),];
+    new Route("/habitat-savane", "habitat-savane", "/pages/habitat-savane.html"),
+    new Route("/habitat-jungle", "habitat-jungle", "/pages/habitat-jungle.html"),
+    new Route("/habitat-marais", "habitat-marais", "/pages/habitat-marais.html"),
+];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
