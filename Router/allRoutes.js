18fa1cbbe43862f7ea7323a826157c2fe6f755f0
LucArronne <lucar.tservice@gmail.com>
2024-10-13 21:21:27 +0200

    finition page service et introduction habitats et habitat-savane

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/service", "Accueil", "/pages/service.html"),];
+    new Route("/service", "Les Services", "/pages/service.html"),
+    new Route("/habitats", "Les Habitats", "/pages/habitats.html"),
+    new Route("/habitat-savane", "habitat-savane", "/pages/habitat-savane.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
