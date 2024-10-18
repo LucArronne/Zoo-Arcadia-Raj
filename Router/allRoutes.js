5a8dd35a5c88e23e756f693551455898349cd1cc
LucArronne <lucar.tservice@gmail.com>
2024-10-18 16:40:07 +0200

    insersion page Avis et connexion sur js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -11,12 +11,13 @@ export const allRoutes = [
     new Route("/restauration", "restauration", "/pages/restauration.html"),
     new Route("/petit-train", "petit-train", "/pages/petit-train.html"),
     new Route("/guide", "guide", "/pages/guide.html"),
-    new Route("/connexion", "connexion", "/pages/connexion.html"),
-    new Route("/avis", "avis", "/pages/avis.html"),
+    new Route("/connexion", "connexion", "/pages/connexion.html", "/js/connexion.js"),
+    new Route("/avis", "avis", "/pages/avis.html", "/js/avis.js"),
     new Route("/contact", "contact", "/pages/contact.html"),
     new Route("/gestionuser", "gestionuser", "/pages/gestionuser.html"),
     new Route("/employé", "employé", "/pages/employé.html"),
     new Route("/veterinaire", "veterinaire", "/pages/veterinaire.html"),
+    new Route("/statistiques", "statistiques", "/pages/statistiques.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
