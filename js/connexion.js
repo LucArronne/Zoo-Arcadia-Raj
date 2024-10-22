fd99e28c32cdb1d96764ff6356e5549ce2db072f
LucArronne <lucar.tservice@gmail.com>
2024-10-22 22:20:34 +0200

    boucler la page de connexion

--- js/connexion.js
+++ js/connexion.js
@@ -1,18 +1,44 @@
+
 const mailInput = document.getElementById("EmailInput");
 const passwordInput = document.getElementById("PasswordInput");
 const btnSignin = document.getElementById("btnSignin");
 
-btnSignin.addEventListener("click", ckeckCredentials);
+btnSignin.addEventListener("click", checkCredentials);
 
-function ckeckCredentials(){
-   // appel Api en bdd// 
-   if (mailInput.value === "administrateur@mail.fr" && passwordInput.value === "123" || mailInput.value === "Employé@mail.fr" && passwordInput.value === "345" || mailInput.value === "vétérinaire@mail.fr" && passwordInput.value === "567"){
+function checkCredentials(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
 
-window.location.replace("/");
-}
-else{
-    mailInput.classList.add ("Identifiants incorrects");
-    passwordInput.classList.add ("Identifiants incorrects");
-    alert("Identifiants incorrects");
-}
+    const credentials = [
+        { email: "Admin@mail.fr", password: "123", role: "admin" },
+        { email: "Employé@mail.fr", password: "345", role: "employe" },
+        { email: "Vétérinaire@mail.fr", password: "567", role: "veterinaire" }
+    ];
+
+    // Recherche si les identifiants sont valides et obtient le rôle
+    const user = credentials.find(cred => 
+        mailInput.value === cred.email && passwordInput.value === cred.password
+    );
+
+    if (user) {
+        switch (user.role) {
+            case "admin":
+                // Redirige l'admin vers la page d'admin
+                window.location.replace("/pages/gestionuser.html");
+                break;
+            case "employe":
+                // Redirige l'employé vers la page Employé
+                window.location.replace("/pages/employé.html");
+                break;
+            case "veterinaire":
+                // Redirige le vétérinaire vers la page vétérianire
+                window.location.replace("/pages/veterinaire.html");
+        } 
+    }
+  
+    else {
+        // Affiche une erreur et ajoute la classe d'erreur sur les champs
+        mailInput.classList.add("is-invalid");
+        passwordInput.classList.add("is-invalid");
+        alert("Identifiants incorrects");
+    }
 }
