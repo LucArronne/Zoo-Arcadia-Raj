7141878e125d250c67ce54d6cbdf60c37736f01f
LucArronne <lucar.tservice@gmail.com>
2024-11-07 15:21:24 +0100

    correction redirection gestionuser

--- js/connexion.js
+++ js/connexion.js
@@ -23,7 +23,7 @@ function checkCredentials(event) {
         switch (user.role) {
             case "admin":
                 // Redirige l'admin vers la page d'admin
-                window.location.replace("/page/index");
+                window.location.replace("/page/gestionuser.html");
                 break;
             case "employe":
                 // Redirige l'employé vers la page Employé
